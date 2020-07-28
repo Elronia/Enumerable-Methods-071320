d1d14a5afe041edf4083a36a2e0782a1d2ba3ff5
Elronia <zar.catherine@gmail.com>
2020-07-28 13:19:19 -0400

    Almost finished

--- run.rb
+++ run.rb
@@ -65,36 +65,24 @@ zoos = {
 zoos["Central Park Zoo"][:animals][2] = {wolves: 4}
 "Change Alex made and saved"
 
-
 # The 2 penguins in the Bronx Zoo just had a baby. Alter the `zoos` information to reflect this new change:
 # Increment the number for the `:count` key in the "Penguin" hash in the Bronx Zoo array by 1. 
 # You can assume that the animal at index 0 will always be the "Penguin" hash.
 zoos["Bronx Zoo"][:animals][0][:count] += 1
 
-
 # Each of the zoos in the city just received 2 pandas. Alter the `zoos` information to reflect this new change:
 # In the array of animals under each Zoo, shovel this hash: { species: "Panda", count: 2 }
-animals = { species: "Panda", count: 2 }
-def add_pandas(animals)
-  counter = 0
-  while counter < zoos.length
-   zoos[counter][:animals].each do |species, count|
-     zoos[counter][:animals] << { species: "Panda", count: 2 }
-    counter += 1
-   end
-  end
-  binding.pry
-  zoos
+zoos.each do |name, zoo_hash|
+    zoo_hash[:animals] << { species: "Panda", count: 2 }
 end
-
 
 # Return the number of Tigers at the Bronx Zoo.
 # Rather than assuming that the animal at index 2 will always be the "Tiger" hash:
 # First find the "Tiger" hash from the array of Animals at the Bronx Zoo and then, access the value under the ":count" key
-
-
-
-
+tiger_hash = zoos["Bronx Zoo"][:animals].find do |animal_hash|
+    animal_hash[:species] == "Tiger" 
+end
+tiger_hash[:count]
 
 
 # Generalize the process to find the ticket price of a specific zoo.
@@ -102,52 +90,49 @@ end
 # Return the price associated with the `name_of_zoo` variable.
 
 # No matter which 1 of the 3 variable assignment you choose, your code should work:
-# name_of_zoo = "Bronx Zoo" => returns 25
+ name_of_zoo = "Bronx Zoo" #=> returns 25
 # name_of_zoo = "Central Park Zoo" => returns 18
 # name_of_zoo = "Staten Island Zoo" => returns 10
 
-
-
-
+price = zoos[name_of_zoo][:price]
+#or
 
 # Return the sum of all the zoos' price. 
 # The return value should be: 53 
-
-
-
-
+total_zoo_prices = zoos.map do |name, zoo_hash|
+  zoo_hash[:price]
+end
+total_zoo_prices.sum
 
 # Return an array of all the locations where the zoos are located.
 # The return value should be an array of strings: ["Bronx", "Manhattan", "Staten Island"]
 # Consider which higher-level enumerable method(s) you'd use here.
-
-
-
-
+zoo_locations = zoos.map do |name, zoo_hash|
+  zoo_hash[:location]
+end
 
 # Find all the zoos that are open on the weekend. 
 # The return value should be a hash with two keys: ["Bronx Zoo", "Central Park Zoo"]
 # Consider which higher-level enumerable method(s) you'd use here.
-
-
-
-
+zoos_open_on_weekend = zoos.select do |name, zoo_hash|
+    zoo_hash[:weekend]
+end
 
 # Find the first zoo that are open on the weekend AND has the price under 20 dollars.
 # The return value should be an array with two elements: ["Central Park Zoo", { location: "Manhattan", ... }]
 # Consider which higher-level enumerable method(s) you'd use here.
-
-
-
-
+weekend_and_less_than_20 = zoos.find do |name, zoo_hash|
+    zoo_hash[:weekend] && zoo_hash[:price] < 20
+end
 
 # Find all the zoos where there are monkeys.
 # The return value should be a hash with two keys: ["Central Park Zoo", "Staten Island Zoo"]
 # Consider which higher-level enumerable method(s) you'd use here.
-
-
-
-
+all_zoos_with_monkeys = zoos.select do |name, zoo_hash|
+    zoo_hash[:animals].find do |animal_hash|
+        animal_hash[:species] == "Monkey"
+    end
+end
 
 # Return an array of 3 numbers, each describing the total sum of the number of animals in a zoo.
 # The return value should be an array of 3 numbers: [20, 14, 17]
@@ -164,3 +149,4 @@ end
 
 
 
+binding.pry
